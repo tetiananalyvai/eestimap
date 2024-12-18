d77199a47b5f8b5a9561b55de5f4dbcc271847c7
Tetiananalyvai <lady.nalivay@gmail.com>
2024-12-18 14:52:41 +0200

    Add project files.

---
+++ MauiEEkaart/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiEEkaart
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); // Цей виклик обов'язковий!
+        }
+    }
+}
